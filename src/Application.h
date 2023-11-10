4047257aa82edc5382a35e386e923151181ee3d5
dcyanikoglu <dcyanikoglu@gmail.com>
2023-11-11 00:34:02 +0300

    Pushing initial support for drawing a window with OpenGL 4.6

---
+++ src/Application.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+class Application
+{
+public:
+	Application() {}
+	Application(const Application&) = delete;
+	Application operator=(const Application&) = delete;
+	
+	virtual ~Application() {}
+	virtual void Initialize() {}
+	virtual void Update(float DeltaTime) {}
+	virtual void Render(float AspectRatio) {}
+	virtual void Shutdown() {}
+};
